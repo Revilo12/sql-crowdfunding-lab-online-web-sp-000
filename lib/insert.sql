3622cb41899bd57c5073fb78d29de6e2196f6bfa
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-02 14:36:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,43 @@
 INSERT INTO users (name, age)
 VALUES ("Bob", 18);
+INSERT INTO users (name, age)
+VALUES ("Abiel", 19);
+INSERT INTO users (name, age)
+VALUES ("Abijah", 20);
+INSERT INTO users (name, age)
+VALUES ("Abimael", 21);
+INSERT INTO users (name, age)
+VALUES ("Abner", 18);
+INSERT INTO users (name, age)
+VALUES ("Abraham", 22);
+INSERT INTO users (name, age)
+VALUES ("Absalom", 22);
+INSERT INTO users (name, age)
+VALUES ("Bazel", 22);
+INSERT INTO users (name, age)
+VALUES ("Benajah", 18);
+INSERT INTO users (name, age)
+VALUES ("Clement", 81);
+INSERT INTO users (name, age)
+VALUES ("Comfort", 81);
+INSERT INTO users (name, age)
+VALUES ("Constant", 81);
+INSERT INTO users (name, age)
+VALUES ("Cotton", 18);
+INSERT INTO users (name, age)
+VALUES ("Cyrus", 32);
+INSERT INTO users (name, age)
+VALUES ("Ebenezer", 32);
+INSERT INTO users (name, age)
+VALUES ("Eleazar", 32);
+INSERT INTO users (name, age)
+VALUES ("Eli", 27);
+INSERT INTO users (name, age)
+VALUES ("Eliab", 27);
+INSERT INTO users (name, age)
+VALUES ("Eliakim", 27);
+INSERT INTO users (name, age)
+VALUES ("Elias", 27);
 
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
